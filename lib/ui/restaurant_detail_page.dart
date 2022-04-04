f1dd4ac274a405f9d98089ec3fb17702cf7e1081
fanolans <mailenolan@gmail.com>
2022-04-05 02:42:20 +0700

    revisi menampilkan data JSON Food dan Drink

--- lib/ui/restaurant_detail_page.dart
+++ lib/ui/restaurant_detail_page.dart
@@ -11,57 +11,107 @@ class RestaurantDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Restaurants App'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Hero(
-                tag: restaurant.id, child: Image.network(restaurant.pictureId)),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Restaurant: ${restaurant.name}',
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                  const SizedBox(height: 10),
-                  Text(
-                    'Location: ${restaurant.city}',
-                    style: Theme.of(context).textTheme.subtitle1,
-                  ),
-                  const SizedBox(height: 10),
-                  Text(
-                    'Rating: ${restaurant.rating}',
-                    style: Theme.of(context).textTheme.subtitle2,
-                  ),
-                  const SizedBox(height: 10),
-                  const Divider(color: Colors.grey),
-                  Text(
-                    'Description: ${restaurant.description}',
-                    style: Theme.of(context).textTheme.bodyText2,
-                  ),
-                  const SizedBox(height: 10),
-                  const Divider(color: Colors.grey),
-                  const SizedBox(height: 10),
-                  Text(
-                    'Foods: ${restaurant.menus.foods.map((e) => e.name)}',
-                    style: Theme.of(context).textTheme.subtitle1,
-                  ),
-                  const SizedBox(height: 10),
-                  Text(
-                    'Drinks: ${restaurant.menus.drinks.map((e) => e.name)}',
-                    style: Theme.of(context).textTheme.subtitle1,
-                  ),
-                ],
-              ),
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text('Restaurants App'),
         ),
-      ),
-    );
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Hero(
+                    tag: restaurant.id,
+                    child: Image.network(restaurant.pictureId)),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Restaurant: ${restaurant.name}',
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        'Location: ${restaurant.city}',
+                        style: Theme.of(context).textTheme.subtitle1,
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        'Rating: ${restaurant.rating}',
+                        style: Theme.of(context).textTheme.subtitle2,
+                      ),
+                      const SizedBox(height: 10),
+                      const Divider(color: Colors.grey),
+                      Text(
+                        'Description: ${restaurant.description}',
+                        style: Theme.of(context).textTheme.bodyText2,
+                      ),
+                      const SizedBox(height: 10),
+                      const Divider(color: Colors.grey),
+                      const SizedBox(height: 10),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          const Icon(
+                            Icons.food_bank,
+                            color: Colors.black,
+                            size: 36.0,
+                          ),
+                          Text('Foods Menu',
+                              style: Theme.of(context).textTheme.subtitle2),
+                          Container(
+                            height: 150,
+                            child: ListView(
+                              scrollDirection: Axis.horizontal,
+                              children: restaurant.menus.foods.map((food) {
+                                return Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Card(
+                                    margin: const EdgeInsets.symmetric(
+                                        vertical: 56.0, horizontal: 4.0),
+                                    child: Text(food.name),
+                                  ),
+                                );
+                              }).toList(),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Divider(color: Colors.grey),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          const Icon(
+                            Icons.local_drink,
+                            color: Colors.black,
+                            size: 36.0,
+                          ),
+                          Text('Drinks Menu',
+                              style: Theme.of(context).textTheme.subtitle2),
+                          Container(
+                            height: 150,
+                            child: ListView(
+                              scrollDirection: Axis.horizontal,
+                              children: restaurant.menus.drinks.map((drink) {
+                                return Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Card(
+                                    margin: const EdgeInsets.symmetric(
+                                        vertical: 56.0, horizontal: 4.0),
+                                    child: Text(drink.name),
+                                  ),
+                                );
+                              }).toList(),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
